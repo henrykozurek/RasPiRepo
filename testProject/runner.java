de703819643caad4279ed81204a3c340345498d7
henrykozurek <hkozurek@gmail.com>
2019-10-23 12:09:39 -0500

    Added testProject

---
+++ testProject/runner.java
@@ -0,0 +1,18 @@
+
+/**
+ * Write a description of class runner here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class runner
+{
+    public static void main (String[] args)
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            String out = "" + i;
+            System.out.println("Hello "+ out);
+        }
+    }
+}
