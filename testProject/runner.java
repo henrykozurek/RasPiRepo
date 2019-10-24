6c8ba74d6a7f96e4dbe734d88e0070c542b24549
henrykozurek <hkozurek@gmail.com>
2019-10-24 10:58:05 -0500

    make note of previous bad change

--- testProject/runner.java
+++ testProject/runner.java
@@ -11,7 +11,7 @@ public class runner
     {
         for (int i = 0; i < 10; i++)
         {
-            String out = "" + i;
+            String out = "" + i; // don't make this plus 1. Just get used to zero-(i-1)
             System.out.println("Hello "+ out);
         }
     }
